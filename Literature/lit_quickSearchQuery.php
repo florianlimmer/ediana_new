48d59bc375a68dfc4bd7cff7f52072c9311aac8b
Katharina Smiatek <smiatek@cip.ifi.lmu.de>
2018-02-01 01:30:36 +0100

    <lit_quickSearchQuery.php> es werden nur 25 results angezeigt, aber paginationsbuttons für alle erzeugt, die jedoch noch nicht anwählbar sind

--- Literature/lit_quickSearchQuery.php
+++ Literature/lit_quickSearchQuery.php
@@ -17,16 +17,46 @@ if(strlen($input) >= 3)
     $i = 0;
     while ($order = mysqli_fetch_assoc($ref_order))
     {
+        if($i <= 25){
+            $output = SQL_reference_output ($order["ref_wpid"], $i);
 
-        $output = SQL_reference_output ($order["ref_wpid"], $i);
+            echo $output;
+            $i++;
+        }
 
-        echo $output;
-        $i++;
+
     }
 
     if (mysqli_num_rows($ref_order)==0) {
         echo "<span class=\"text-muted\"> No results found. </span>";
     }
+
+    $number = mysqli_num_rows($ref_order);
+
+    echo"
+    <nav aria-label=\"Page navigation top\">
+    <ul class=\"pagination pagination-sm justify-content-end\">
+        <li class=\"page-item\">
+            <a class=\"page-link\" href=\"#\" aria-label=\"Previous\">
+                <span aria-hidden=\"true\">&laquo;</span>
+                <span class=\"sr-only\">Previous</span>
+            </a>
+        </li>";
+
+    for($i=1; $i<=$number/25; $i++){
+        echo" <li class='page-item'><a class='page-link' href=''>";
+        echo $i;
+        echo"</a></li>";
+    }
+    echo"
+        <li class=\"page-item\">
+            <a class=\"page-link\" href=\"#\" aria-label=\"Next\">
+    <span aria-hidden=\"true\">&raquo;</span>
+                <span class=\"sr-only\">Next</span>
+            </a>
+        </li>
+    </ul>
+    </nav>";
 
     //echo "<script type='text/javascript' src='BIBLIOGRAPHY/JAVA_scripts/lit_lemma.js'></script>";
 }
--- literature_main.php
+++ literature_main.php
@@ -113,25 +113,7 @@ include "Literature/lit_slider.php"
 ?>
 
 
-<nav aria-label="Page navigation top">
-    <ul class="pagination pagination-sm justify-content-end">
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
- <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-        </li>
-    </ul>
-</nav>
+
 
 <div id="results" data-children=".card-footer">
 
